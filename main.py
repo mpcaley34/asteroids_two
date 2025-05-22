bb224924200f2b38c4a37dd3793d1506afd5a063
mpcaley34 <mcaley34@gmail.com>
2025-05-22 15:29:45 -0400

    player and asteroids made with collision logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import (
     SCREEN_WIDTH,
@@ -7,20 +8,49 @@ from constants import (
     ASTEROID_KINDS,
     ASTEROID_SPAWN_RATE
 )
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    time = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    astro_field = AsteroidField()
+
     while True:
-        screen.fill('black')
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+        screen.fill('black')
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()  # CALL THIS LAST
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+
+        # limit the framerate to 60 FPS
+        dt = time.tick(60) / 1000
 
 
 if __name__ == "__main__":
